4f70ffb2d74915430ea12df49f5f6730fbf4c26e
Gerson santos <jhxn07@gmail.com>
2022-01-06 23:23:23 -0600

    uso de firebase para cargar datos dinamicamente mediente el proceso de servicio

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,16 +8,30 @@ import { InfoPagina } from '../interfaces/info-pagina-interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
     console.log('Servicio de info listo');
+    this.cargaInfo();
+    this.cargaEquipo();
+  }
 
+  private cargaInfo() {
     // Leer un archivo JSON
-    this.http.get('assets/data/data-pages.json').subscribe((resp: InfoPagina) => {
-      this.cargada = true;
-      this.info = resp;
+    this.http
+      .get('assets/data/data-pages.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
 
-      console.log(resp);
-    });
+  private cargaEquipo() {
+    this.http
+      .get('https://ng-portafolio-ebd80-default-rtdb.firebaseio.com/.json')
+      .subscribe((resp: any) => {
+        this.equipo = resp;
+        console.log(resp)
+      });
   }
 }
