735886fc6937d112ca205016f29d65d44d72ffc1
Gerson santos <jhxn07@gmail.com>
2022-01-06 19:22:20 -0600

    Creacion de servicios e interfaces

---
+++ src/app/interfaces/info-pagina-interface.ts
@@ -0,0 +1,12 @@
+
+export interface InfoPagina {
+    titulo?: string;
+    email?: string;
+    nombre?: string;
+    fanpages?: string;
+    facebook?: string;
+    twitter?: string;
+    instagram?: string;
+    tumblr?: string;
+    equipo_trabajo?: any[];
+}
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { InfoPagina } from '../interfaces/info-pagina-interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InfoPaginaService {
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor(private http: HttpClient) {
+    console.log('Servicio de info listo');
+
+    // Leer un archivo JSON
+    this.http.get('assets/data/data-pages.json').subscribe((resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+
+      console.log(resp);
+    });
+  }
+}
