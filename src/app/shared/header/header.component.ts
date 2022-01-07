327c7b0a18a8d8e5b0c8bafa9440e1a83ff56327
Gerson santos <jhxn07@gmail.com>
2022-01-06 20:14:38 -0600

    se rempleza href por servicio

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(public _infService: InfoPaginaService) {}
 
   ngOnInit(): void {}
 }
