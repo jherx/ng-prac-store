f0642a809f36cc6591006fedb86f0fd359c1310e
Gerson santos <jhxn07@gmail.com>
2022-01-07 23:15:32 -0600

    actualizacion de productos y remplazo de imagenes

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -9,22 +9,30 @@ import { ProductoService } from 'src/app/services/producto.service';
   styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
-
+  producto: ProductoDescripcion = {};
   cargando = true;
+  id: string | undefined;
 
-  constructor(private route: ActivatedRoute,
-    public productoService: ProductoService) {}
+  constructor(
+    private route: ActivatedRoute,
+    public productoService: ProductoService
+  ) {}
 
+  fecha = new Date();
   ngOnInit(): void {
     this.route.params.subscribe((parametros) => {
       setTimeout(() => {
         this.cargando = false;
       }, 50000);
-      
-      this.productoService.getProducto(parametros['id'])
-      .subscribe((producto: ProductoDescripcion) => {
-        console.log(producto);
-      })
+
+      this.productoService
+        .getProducto(parametros['id'])
+        .subscribe((producto: ProductoDescripcion) => {
+          console.log(producto);
+
+          this.id = parametros['id'];
+          this.producto = producto;
+        });
     });
   }
 }
